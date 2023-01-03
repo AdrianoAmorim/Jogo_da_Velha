68a2262c85a57f7c196c76d2b1ca9bd9dc09b2de
Adriano Amorim <adryanoamorim@hotmail.com>
2023-01-02 22:44:29 -0300

    audio de acoes do jogo concluido

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ const formJogador = document.querySelector("#boxJogadorVez #formJgd");
 const componentJgd1 = document.getElementById("jgd1")
 const componentJgd2 = document.getElementById("jgd2")
 const audioJogada = document.getElementById("audioJogada")
+const audioVitoria = document.getElementById("audioVitoria")
+const audioEmpate = document.getElementById("audioEmpate")
 
 //posicoes que dao vitoria
 let posicoes = [
@@ -20,8 +22,8 @@ let posicoes = [
 let formJgd1 = "X";
 let formJgd2 = "O";
 let jogador1 = localStorage.getItem("jogador1");
-let jogador2 =  localStorage.getItem("jogador2");
-let jogadorAtual ="";
+let jogador2 = localStorage.getItem("jogador2");
+let jogadorAtual = "";
 let formAtual = ""
 let opcSelecionadas;
 
@@ -29,11 +31,11 @@ let opcSelecionadas;
 //Função para inicializar o jogo - zera os buttons e seta o nome do primeiro jogador
 const inicializar = () => {
     //DIMINUI O SOM DE JOGADA 
-    if(audioJogada.volume > 0.8){
+    if (audioJogada.volume > 0.8) {
         audioJogada.volume -= 0.8;
     }
     //usada para guardar as opções ja marcadas no jogo
-    opcSelecionadas =[];
+    opcSelecionadas = [];
     //seta o nome e a forma do jogador da vez
     nomeJogador.textContent = jogador1;
     formJogador.textContent = formJgd1;
@@ -42,55 +44,63 @@ const inicializar = () => {
     componentJgd1.textContent = jogador1
     componentJgd2.textContent = jogador2
     //seta o evento de clique para todos os botoes do jogo e atribue a funcao de novaJogada
-    document.querySelectorAll("#tabelaGame button").forEach((item)=>{
+    document.querySelectorAll("#tabelaGame button").forEach((item) => {
         item.innerHTML = ""
         item.classList.remove("efeitoMarcacao");
-        item.addEventListener("click",novaJogada)
+        item.addEventListener("click", novaJogada)
     })
 }
 //
-const novaJogada = (e)=>{
+const novaJogada = (e) => {
     audioJogada.play();
     e.target.innerHTML = formAtual;
     const index = e.target.getAttribute("id").split("n")[1];
     e.target.classList.add("efeitoMarcacao");
-    e.target.removeEventListener("click",novaJogada)
+    e.target.removeEventListener("click", novaJogada)
     opcSelecionadas[index] = formAtual;
-    
+
     //Faz a checkagem da Jogada se tem o ganhador ou empate
-    setTimeout(()=>{
+    setTimeout(() => {
         check();
-    },[300]);
-     //passamos a vez do jogador e mostramos ao usuario de quem é a vez
-     formJogador.textContent = formAtual === formJgd1 ? formJgd2:formJgd1
-     nomeJogador.textContent = jogadorAtual === jogador1 ? jogador2 : jogador1
-     formAtual = formAtual === formJgd1 ? formJgd2:formJgd1
-     jogadorAtual = jogadorAtual === jogador1 ? jogador2 : jogador1
+    }, [300]);
+    //passamos a vez do jogador e mostramos ao usuario de quem é a vez
+    formJogador.textContent = formAtual === formJgd1 ? formJgd2 : formJgd1
+    nomeJogador.textContent = jogadorAtual === jogador1 ? jogador2 : jogador1
+    formAtual = formAtual === formJgd1 ? formJgd2 : formJgd1
+    jogadorAtual = jogadorAtual === jogador1 ? jogador2 : jogador1
 }
 
-const check =()=>{
-    const auxFormAtual = formAtual === formJgd1 ? formJgd2:formJgd1
+const check = () => {
+    const auxFormAtual = formAtual === formJgd1 ? formJgd2 : formJgd1
     const auxjogadorAtual = jogadorAtual === jogador1 ? jogador2 : jogador1
     //Faz a verificao se ha um ganhador
     const items = opcSelecionadas
-    .map((item,i)=>[item,i])
-    .filter((item)=>item[0] === auxFormAtual)
-    .map((item)=>item[1])
+        .map((item, i) => [item, i])
+        .filter((item) => item[0] === auxFormAtual)
+        .map((item) => item[1])
     //verifica se bate as posicoes ja jogada com as opcoes do array 
-    for(pos of posicoes){
-        if(pos.every((item)=> items.includes(item))){
-            alert(`${auxjogadorAtual} ganhou!!` )
-            inicializar()
-            return
+    for (pos of posicoes) {
+        if (pos.every((item) => items.includes(item))) {
+            audioVitoria.play();
+            setTimeout(() => {
+                alert(`${auxjogadorAtual} ganhou!!`)
+                inicializar()
+                return
+            }, [100]);
+
         }
     }
     //faz a verificação se houve um empate 
-    if(opcSelecionadas.filter((item)=>item).length ===9){
-        alert("OPS! Houve Um empate, Vamos de novo!");
-        inicializar();
-        return;
+    if (opcSelecionadas.filter((item) => item).length === 9) {
+        audioEmpate.play()
+        setTimeout(() => {
+            alert("OPS! Houve Um empate, Vamos de novo!");
+            inicializar();
+            return;
+        }, [100])
+
     }
-  
+
 
 }
 
